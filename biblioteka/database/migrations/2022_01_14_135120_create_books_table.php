a7de2065946769e9c390b5fbc188489d49f50e30
Aleksandra Kalimancevic <ak20180179@student.fon.bg.ac.rs>
2022-01-14 15:02:31 +0100

    Kreirane migracije

---
+++ biblioteka/database/migrations/2022_01_14_135120_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique();
+            $table->string('title');
+            $table->string('excerpt');
+            $table->string('body');
+            $table->timestamps();
+            $table->timestamp('pusblished_at')->nullable();
+            //$table->foreignId('author_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
