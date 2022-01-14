853e5f96ff28813726b34cfbdf585021abec7246
Aleksandra Kalimancevic <ak20180179@student.fon.bg.ac.rs>
2022-01-14 16:12:22 +0100

    Dodati fajlovi AuthorFactory i BookFactory

---
+++ biblioteka/database/factories/BookFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title(),
+            'description' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'user_id' => User::factory(),
+            'author_id' => Author::factory(),
+        ];
+    }
+}
